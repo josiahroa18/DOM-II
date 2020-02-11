ab283fbadcdfe4815c5c657a8ddd94369ea80168
Josiah Roa <josiahroa18@gmail.com>
2020-02-11 13:08:14 -0700

    Added 3 event handlers

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+
+// 1. Make nav links grow on 'mouseenter'
+const navLinks = document.querySelectorAll('.nav a');
+navLinks.forEach(a => {
+    a.addEventListener('mouseenter', () => {
+        a.style.transform = 'scale(1.1)';
+    });
+});
+
+// 2. Make nav links shrink on 'mouseleave'
+navLinks.forEach(a => {
+    a.addEventListener('mouseleave', () => {
+        a.style.transform = 'scale(1)';
+    })
+})
+
+// 3. Make img large or small on 'doubleclick'
+const img = document.querySelectorAll('img');
+img.forEach(img => {
+    img.addEventListener('dblclick', () => {
+        img.style.transform = img.style.transform === 'scale(1.1)' ? 'none' : 'scale(1.1)';
+    })
+})
+
+// 4. Make background change on 'scroll'
+const body = document.querySelector('body');
